0646c3a7036d7bd55176f831ef955472082831f9
HHM-1999 <hhm639069@gmail.com>
2022-09-29 19:51:47 +0530

    9th commit

--- src/Componants/calculation/Calculation.js
+++ src/Componants/calculation/Calculation.js
@@ -1,26 +1,54 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import image from '../../Images/Rectangle 19.png';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Calculation = (props) => {
+    
     const [calculate, setCalculate] = useState(0);
+    
 
     const handlebutton = (id) => {
         // console.log(id);
         const calculate = id;
         setCalculate(calculate);
+        localStorage.setItem('BreakTime', calculate);
     }
+
+    ///TOAST
     const notify = () => {
         toast("Well Done !!!!!! Activation Successful.....");
     }
+
+    // ///LOCAL  sTORAGE
+
+    useEffect(() => {
+        const getBreakTime = localStorage.getItem('BreakTime');
+        setCalculate(getBreakTime ? getBreakTime: 0);
+    },[])
+
+    // const getTime = () => {
+    //     let time = localStorage.getTime('BreakTime');
+    //     console.log(time);
+    //     if (time) {
+    //         return JSON.parse(localStorage.getItem('BreakTime'));
+    //     }
+    //     else {
+    //         return [];
+    //     }
+    //     getTime();
+    // }
+
+    // useEffect(() => {
+    //     localStorage.setItem('BreakTime ', JSON.stringify(calculate));
+    // }, [calculate]);
 
     return (
         <div className='Drawer container'>
                 <div>
                    
                 <img src={image} alt=""></img>
-                     <h4 className='m-0'> Humaira Hossain </h4>
+                <h4 className='m-0'> Humaira Hossain </h4>
                 </div>
                 <span>Junior Web Devoloper</span>
                 
@@ -36,17 +64,17 @@ const Calculation = (props) => {
                     <h5 className='mt-5'>Exercise Details</h5>
                     <div>
                         <span className='text-primary mt-5'>Exercise Time :</span>
-                    <span>{props.duration}</span>
+                    <span>{props.duration}h</span>
                     </div>
                       <div>
                         <span className='text-primary mt-5'>Break Time : </span>
-                    <span>{calculate}</span>
+                    <span>{calculate}s</span>
                     </div>
                       <div className='mt-5'>
                     <button className='btn btn-primary text-white rounded-5 p-3' onClick={notify} >Activity Completed</button>
                       <ToastContainer />
                        
-                                       </div>
+                            </div>
                      
                 
 
